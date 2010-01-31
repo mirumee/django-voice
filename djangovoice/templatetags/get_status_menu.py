1ee5c559f3e1d05287cbf30f78f66cae0af6c404
huwshimi <huwshimi@9b1ad702-0ae9-11df-90cd-0fbaec372790>
2010-01-31 03:44:44 +0000

    Show appropriate statuses in sidebar for context (if viewing closed feedback only show closed statuses)
    git-svn-id: http://django-voice.googlecode.com/svn/trunk@6 9b1ad702-0ae9-11df-90cd-0fbaec372790

--- djangovoice/templatetags/get_status_menu.py
+++ djangovoice/templatetags/get_status_menu.py
@@ -1,17 +1,31 @@
 from djangovoice.models import Status
-from django.template import Library,Node
+from django.template import Library, Variable, TemplateSyntaxError, Node
 
 register = Library()
+
+class StatusObject(Node):
+    def __init__(self, list):
+        self.list = Variable(list)
+    
+    def render(self, context):
+        list = self.list.resolve(context)
+        status_list = Status.objects.all()
+        
+        if list == "open":
+            status_list = status_list.filter(status="open")
+        elif list == "closed":
+            status_list = status_list.filter(status="closed")
+        
+        context['status_list'] = status_list
+        return ''
 
 def build_status_list(parser, token):
     """
     {% get_status_list %}
     """
-    return StatusObject()
-
-class StatusObject(Node):
-    def render(self, context):
-        context['status_list'] = Status.objects.all()
-        return ''
+    bits = token.contents.split()
+    if len(bits) != 2:
+        raise TemplateSyntaxError, "'%s' tag takes exactly 1 arguments" % bits[0]
+    return StatusObject(bits[1])
 
 register.tag('get_status_list', build_status_list)
